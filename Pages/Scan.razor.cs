f23610d4b4a0d3ac192c3211d0af45420e4b532e
ers <erik@steini.at>
2022-01-09 08:57:23 +0100

    wip

---
+++ Pages/Scan.razor.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using Blazm.Bluetooth;
+using Microsoft.AspNetCore.Components;
+
+namespace VariaLogger.Pages
+{
+    public partial class Scan
+    {
+        [Inject] IBluetoothNavigator navigator { get; set; }
+
+        protected async Task StartScanning()
+        {
+            var q = new RequestDeviceQuery();
+            q.AcceptAllDevices = true;
+            var device = await navigator.RequestDeviceAsync(q);
+
+        }
+    }
+}
