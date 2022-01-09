849ddb7dceff428eada4eb202fbb2635b91ff31d
ers <erik@steini.at>
2022-01-09 08:34:23 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazm.Bluetooth;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VariaLogger;
@@ -5,6 +6,8 @@ using VariaLogger;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazmBluetooth();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
