be0b8b6e009839ba4fbde91541bf6e17254512c8
ers <erik@steini.at>
2022-01-09 10:58:07 +0100

    wip

--- Pages/Scan.razor.cs
+++ Pages/Scan.razor.cs
@@ -8,14 +8,6 @@ namespace VariaLogger.Pages
 {
     public partial class Scan
     {
-        [Inject] IBluetoothNavigator navigator { get; set; }
-
-        protected async Task StartScanning()
-        {
-            var q = new RequestDeviceQuery();
-            q.AcceptAllDevices = true;
-            var device = await navigator.RequestDeviceAsync(q);
-
-        }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 using Blazm.Bluetooth;
+using VariaLogger;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using VariaLogger;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazmBluetooth();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazmBluetooth();
 
 await builder.Build().RunAsync();
